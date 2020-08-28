5bc37559d81a2d62631de6df63950c4bf918e42f
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-08-28 18:55:57 -0300

    exercise 03

---
+++ Introduction-Web_Development/block-04/day-02/exercise-03.js
@@ -0,0 +1,13 @@
+// Calculating the arithmetic mean
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum = sum + numbers[i];
+}
+
+let mean = sum / (numbers.length)
+
+console.log(mean);
