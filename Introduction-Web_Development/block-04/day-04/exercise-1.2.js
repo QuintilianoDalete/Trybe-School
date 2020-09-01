edb4db63ece3b4cd98e4cb5d616a03176d0209bb
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-01 19:13:44 -0300

    Until exercise 1.5

---
+++ Introduction-Web_Development/block-04/day-04/exercise-1.2.js
@@ -0,0 +1,11 @@
+// Adding a property and printing all the object
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  info.recorrente = "Sim";
+
+  console.log(info);
