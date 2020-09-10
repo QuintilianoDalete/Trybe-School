46c9ce90ea9bce266063f3154eb5d1d2bd4bdafb
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-10 15:55:55 -0300

    Exercise 1

--- tests.js
+++ tests.js
@@ -1,66 +1,19 @@
-// Desafio 13
-function hydrate(string) {
-    let array = Array.from(string);
-    let result = [];
-    let number = 0;
-    let final = 0;
-
+function decode(string) {
+    let array = string.split('');
     for (let i = 0; i < array.length; i += 1) {
-
-        if (array[i] === '1') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-
-        } else if (array[i] === '2') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-        } else if (array[i] === '3') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-        } else if (array[i] === '4') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-        
-        } else if (array[i] === '5') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-
-        } else if (array[i] === '6') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-            
-        } else if (array[i] === '7') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-            
-        } else if (array[i] === '8') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-            
-        } else if (array[i] === '9') {
-            number = Number(array[i]);
-            result.push(number);
-            number = 0;
-        }
+      if (array[i] == 1) {
+        array[i] = 'a';
+      } else if (array[i] == 2) {
+        array[i] = 'e';
+      } else if (array[i] == 3) {
+        array[i] = 'i';
+      } else if (array[i] == 4) {
+        array[i] = 'o';
+      } else if (array[i] == 5) {
+        array[i] = 'u';
+      }
     }
-    for ( let j = 0; j < result.length; j += 1) {
-        final = final + result[j];
-    }
-    
-    if (final === 1) {
-        return `${final} copo de água`;
+    return array.join('');
+  }
 
-    } else {
-        return `${final} copos de água`;
-    }
-}
-
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
+  console.log(decode("How 1re you tod1y?"));
